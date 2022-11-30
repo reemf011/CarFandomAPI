8980b2a37ac531fd7bd7df0a9b0c72e0134d24f1
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-11-30 11:56:37 +0300

    initialized project

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const express = require(`express`);
+const dotenv = require(`dotenv`);
+
+const initiateDBConnection = require(`./config/db`); 
+
+dotenv.config({
+    path: `./config/.env`
+});
+
+const PORT =process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, async () => {
+  console.log(`server has been started as is listening to port ${PORT}`);
+  await initiateDBConnection();
+});
