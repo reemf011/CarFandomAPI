e7f62d7cb2877d92f315b23a1ec688873330c4ca
Farah Dorgham <farahwael01@gmail.com>
2022-12-01 12:20:14 +0200

    farah update

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,7 +1,7 @@
 const customerservice=require(`../services/customer`);
 module.exports.getCustomer= async(req,res)=>{
     try{
-        const customer= await customerservice.findAllProducts();
+        const customer= await customer.findAllProducts();
         res.send({customer});
     }catch(err){
         res.status(500);
--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,5 @@
 const {Router}=require(`express`);
-const customercontroller=require(`../controllers/Customer`);
+const customercontroller=require(`../controllers/customer`);
 const customerrouter=Router();
 customerrouter.get(`/`,customercontroller,customercontroller.getCustomer);
 module.exports=customerrouter;
