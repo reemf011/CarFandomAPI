7891e37b1c2ae1ced852ea8dd47e387eaab13d7d
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-12-01 22:04:28 +0300

    reem update

--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,5 @@
-const {Router}=require(`express`);
-const customercontroller=require(`../controllers/Customer`);
+const {Router}=require('express');
+const customerController=require('../controllers/customer');
 const customerrouter=Router();
-customerrouter.get(`/`,customercontroller.getCustomer);
+customerrouter.get('/',customerController.getCustomer);
 module.exports=customerrouter;
