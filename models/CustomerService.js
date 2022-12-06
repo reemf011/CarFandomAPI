f59fafd9a3642c8199b8bcddaec4aceec653e271
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-12-06 16:35:14 +0300

    reem's

--- models/CustomerService.js
+++ models/CustomerService.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose');
 
 const CustomerServiceSchema = new Schema({
-    custServiveID:{
+    custServiceID:{
         type:'Number',
         required: true
     },
